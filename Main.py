71adfa15b790afe65dc96c9b56b8f96c3c3c45ea
knoopsr <107144849+knoopsr@users.noreply.github.com>
2026-01-21 20:11:11 +0100

    update crud

--- Main.py
+++
@@ -1,1 +0,0 @@
-hghvhgc
---
+++ crud_functies.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="syntra",
+    password="Syntra$2026",
+    ddatabase="syntra"
+)
+
+
+
+
+
+def create_company(name, vat_number):
+
+def list_companies():
+
+def get_company(company_id):
+
+def update_company(company_id, name, vat_number):
+
+def delete_company(company_id):
+
+def create_contact(company_id, name, email, phone):
+
+def list_contacts(company_id=None):
+
+def get_contact(contact_id):
+
+def update_contact(contact_id, name, email, phone):
+
+def delete_contact(contact_id):
+
+
+
